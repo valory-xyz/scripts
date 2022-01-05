81b9392f09bc54344696aa9107cf1aa18e2dbb2e
Adamantios <adamantios.zaras@valory.xyz>
2022-01-05 17:32:28 +0200

    [feat]: create configurations

---
+++ spookyswap-apy/config.py
@@ -0,0 +1,9 @@
+"""Project configurations."""
+
+_subgraph_base_url = 'https://api.thegraph.com/subgraphs/name/'
+SPOOKY_SUBGRAPH_URL = _subgraph_base_url + 'eerieeight/spookyswap'
+FANTOM_BLOCKS_SUBGRAPH_URL = _subgraph_base_url + 'matthewlilley/fantom-blocks'
+BUNDLE_ID = 1
+MINUTE_IN_UNIX = 60
+HOUR_IN_UNIX = 60 * MINUTE_IN_UNIX
+DAY_IN_UNIX = 24 * HOUR_IN_UNIX
